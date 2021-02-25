ce3cff41b59b74e77c5758a5a889763b2b24508e
erneston666 <31527713+erneston666@users.noreply.github.com>
2021-02-25 03:08:33 -0800

    check

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,36 @@
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { APP_ROUTING } from './app.routes';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    FooterComponent,
+    InicioComponent,
+ 
   ],
   imports: [
     BrowserModule,
+    APP_ROUTING
     
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    
+  ],
+  bootstrap: [AppComponent],
+  schemas: [
+    NO_ERRORS_SCHEMA
+    ]
 })
 export class AppModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
+import { LoginComponent } from "./components/login/login.component";
+import { InicioComponent } from "./components/inicio/inicio.component";
 
 
 const APP_ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'inicio', component: InicioComponent },
 
 
   { path: '**', pathMatch: 'full', redirectTo: '' }
